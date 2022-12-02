b95b89806f9cc206710e1fab573b87160be823fe
iAmJitenderRawat <jitenderrawat39@gmail.com>
2022-12-03 01:07:32 +0530

    first commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,52 +1,57 @@
-import { Card, CardHeader, Grid, Heading, Text,Box, CardBody, Image, CardFooter, Button } from '@chakra-ui/react'
+import { Grid, Heading,Box, Image, GridItem, Text } from '@chakra-ui/react'
 import React from 'react'
-import { BiLike, BiChat, BiShare } from "react-icons/bi";
+
 
 export const Projects = () => {
   return (
     <Box className="p-15">
       <Heading className="center ptb-10">Project Section</Heading>
-      <Grid>
-        <Card maxW="md">
+      <Grid templateColumns={"repeat(3,1fr)"} gap={"8"}>
+        <GridItem className="card">
           <Box>
-            <Image
-              objectFit="cover"
-              src="https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-              alt="Chakra UI"
-            />
+            <Image src="../Images/calculator.png" alt="calculator" />
           </Box>
-          <CardHeader>
-              <Box>
-                <Heading size="sm">Segun Adebayo</Heading>
-              </Box>
-          </CardHeader>
-          <CardBody>
-            <Text>
-              With Chakra UI, I wanted to sync the speed of development with the
-              speed of design. I wanted the developer to be just as excited as
-              the designer to create a screen.
-            </Text>
-          </CardBody>
-          <CardFooter
-            justify="space-between"
-            flexWrap="wrap"
-            sx={{
-              "& > button": {
-                minW: "136px",
-              },
-            }}
-          >
-            <Button flex="1" variant="ghost" leftIcon={<BiLike />}>
-              Like
-            </Button>
-            <Button flex="1" variant="ghost" leftIcon={<BiChat />}>
-              Comment
-            </Button>
-            <Button flex="1" variant="ghost" leftIcon={<BiShare />}>
-              Share
-            </Button>
-          </CardFooter>
-        </Card>
+          <Heading fontSize={"xl"} className="m-10 center">
+            Calculator
+          </Heading>
+          <Text>It was my first basic working calculator application. </Text>
+          <Heading fontSize={"lg"}>Tech Stack</Heading>
+          <Grid templateColumns={"repeat(3,1fr)"} gap="5">
+            <GridItem>HTML</GridItem>
+            <GridItem>CSS</GridItem>
+            <GridItem>JAVASCRIPT</GridItem>
+          </Grid>
+        </GridItem>
+        <GridItem className="card">
+          <Box>
+            <Image src="../Images/calculator.png" alt="calculator" />
+          </Box>
+          <Heading fontSize={"xl"} className="m-10 center">
+            Calculator
+          </Heading>
+          <Text>It was my first basic working calculator application. </Text>
+          <Heading fontSize={"lg"}>Tech Stack</Heading>
+          <Grid templateColumns={"repeat(3,1fr)"} gap="5">
+            <GridItem>HTML</GridItem>
+            <GridItem>CSS</GridItem>
+            <GridItem>JAVASCRIPT</GridItem>
+          </Grid>
+        </GridItem>
+        <GridItem className="card">
+          <Box>
+            <Image src="../Images/calculator.png" alt="calculator" />
+          </Box>
+          <Heading fontSize={"xl"} className="m-10 center">
+            Calculator
+          </Heading>
+          <Text>It was my first basic working calculator application. </Text>
+          <Heading fontSize={"lg"}>Tech Stack</Heading>
+          <Grid templateColumns={"repeat(3,1fr)"} gap="5">
+            <GridItem>HTML</GridItem>
+            <GridItem>CSS</GridItem>
+            <GridItem>JAVASCRIPT</GridItem>
+          </Grid>
+        </GridItem>
       </Grid>
     </Box>
   );
